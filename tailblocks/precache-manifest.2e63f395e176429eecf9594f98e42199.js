930e0b60888cdbab274c8eb7b8fe72d891417773
Ravi D <ravi1917d@gmail.com>
2020-05-16 16:19:06 +0530

    tailblocks added

---
+++ tailblocks/precache-manifest.2e63f395e176429eecf9594f98e42199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4c03c46d64751607b8b40a0591953d",
+    "url": "/tailblocks/index.html"
+  },
+  {
+    "revision": "0d84ea5641738c5b61b4",
+    "url": "/tailblocks/static/css/main.ddf81824.chunk.css"
+  },
+  {
+    "revision": "f120907f0e695961d63b",
+    "url": "/tailblocks/static/js/2.4f9f66ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tailblocks/static/js/2.4f9f66ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d84ea5641738c5b61b4",
+    "url": "/tailblocks/static/js/main.ad4bb8ac.chunk.js"
+  },
+  {
+    "revision": "6b42aa5fb4b4d5910b21",
+    "url": "/tailblocks/static/js/runtime-main.ad462f65.js"
+  }
+]);
